481c42e17f719b24027015263096962daf018203
Maria Moseshvili <93208114+sukkie95@users.noreply.github.com>
2022-02-06 16:44:24 +0400

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,34 +1,34 @@
 import React from "react";
 
-
-const Card = ({userData, userInput}) => {
-    return ( 
-   <div className="main">
-     {userData.filter((val)=>{
-      if(userInput==='' ){
-        return ''
-      } else if(val.login.startsWith(userInput.toLowerCase())){
-        return val
-      }
-    
-
-    }).map((val, key)=>(
-      <a href={val.html_url} target="blank">
-    <div className="card" key='key'>
-    
-      <div className="image">
-        <div><img src={val.avatar_url} /></div>
-        <div><p>{val.login}</p></div> </div>
-      
-
+const Card = ({ userData, userInput }) => {
   
+  if(!userData)return ''
+  return (
+    <div className="main">
+      {userData.items
+        .filter((val) => {
+          if (userInput === "") {
+            return "";
+          } else if (val.login.startsWith(userInput.trim().toLowerCase())) {
+            return val;
+          }
+        })
+        .map((val, key) => (
+          <a href={val.html_url} target="blank" key={val.login}>
+            <div className="card" key="key">
+              <div className="image">
+                <div>
+                  <img src={val.avatar_url} alt="profile" />
+                </div>
+                <div>
+                  <p>{val.login}</p>
+                </div>
+              </div>
+            </div>
+          </a>
+        ))}
     </div>
-    </a>
-    ))}
-       
-         
-   </div>
-    )
-}
- 
+  );
+};
+
 export default Card;
